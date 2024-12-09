610012111306a14366cc1b36e16f8e967be1c9f3
mc-thay <122173225+mc-thay@users.noreply.github.com>
2024-12-08 19:41:58 -0500

    Completo con m3, 2.1

--- app/src/main/java/com/aguilar/controlacceso/MainActivity.kt
+++ app/src/main/java/com/aguilar/controlacceso/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-
+//
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
